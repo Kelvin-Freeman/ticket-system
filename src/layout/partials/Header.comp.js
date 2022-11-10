4d86d998644651c5623595174e817b47b35726ae
Kelvin Freeman <107550014+Kelvin-Freeman@users.noreply.github.com>
2022-11-10 08:48:48 -0500

    navbar created

--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {Navbar, Nav} from 'react-bootstrap'
+import logo from '../../assets/img/ticketlogo.png'
 
 export const Header = () => {
     return(
-        <div>
-            This is the Header
-        </div>
+      <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
+                
+                <Navbar.Brand>
+                    <img src={logo}alt="logo" width="50px" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="/dashboard">Tickets</Nav.Link>
+                        <Nav.Link href="/dashboard">Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+          
+            
     )
 }
