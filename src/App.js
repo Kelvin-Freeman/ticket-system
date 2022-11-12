b8627d6187b8ef66bf04460ff9e49160b48605d5
Kelvin Freeman <107550014+Kelvin-Freeman@users.noreply.github.com>
2022-11-12 10:09:47 -0500

    routes added, still working on them

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 
 import React from 'react'
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import {DefaultLayout} from '../../Ticket-System/src/layout/DefaultLayout'
-// import {Dashboard} from './pages/dashboard/Dashboard.page'
-// import { Entry } from  '../../Ticket-System/src/pages/entry/Entry.page'
-// import { AddTicket } from './pages/new-ticket/AddTicket.page';
-// import { TicketLists } from './pages/ticket-list/TicketLists.page';
+import {Dashboard} from './pages/dashboard/Dashboard.page'
+import { Entry } from  '../../Ticket-System/src/pages/entry/Entry.page'
+import { AddTicket } from './pages/new-ticket/AddTicket.page';
+import { TicketLists } from './pages/ticket-list/TicketLists.page';
 import {Ticket} from '../src/pages/ticket/Ticket.page'
 
 function App() {
   
   return (
-   <div className="App">
-    {/* <Entry /> */}
-    <DefaultLayout>
-      
-      {/* <Dashboard /> */}
-      {/* <AddTicket /> */}
-      {/* <TicketLists /> */}
-      <Ticket />
-    </DefaultLayout>
+    <div className="App">
+    
+      <>
+      <DefaultLayout>
+          <Routes>
+            <Route path="/" element={<Entry />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/add-ticket" element={<AddTicket />} />
+            <Route path="/tickets" element={<TicketLists />} />
+            <Route path="/ticket/:tid" element={<Ticket />} />
+            </Routes>
+        </DefaultLayout>
+      </>
+   
     </div>
   );
 }
+
 
 export default App;
--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Navbar, Nav} from 'react-bootstrap'
 import logo from '../../assets/img/ticketlogo.png'
-// import { LinkContainer } from "react-router-bootstrap"
+import { Link } from "react-router-dom"
 
 export const Header = () => {
     return(
@@ -14,9 +14,12 @@ export const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                        <Link to="/dashboard">Dashboard</Link>
+                        <Link to="/dashboard">Tickets</Link>
+                        <Link to="/dashboard">Logout</Link>
+                        {/* <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                         <Nav.Link href="/dashboard">Tickets</Nav.Link>
-                        <Nav.Link href="/dashboard">Logout</Nav.Link>
+                        <Nav.Link href="/dashboard">Logout</Nav.Link> */}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
