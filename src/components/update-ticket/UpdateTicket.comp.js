71fb591b4a52876c88e24dc854f68ad7418e3858
Kelvin Freeman <107550014+Kelvin-Freeman@users.noreply.github.com>
2022-11-12 08:41:43 -0500

    ticket landing page done!

--- src/components/update-ticket/UpdateTicket.comp.js
+++ src/components/update-ticket/UpdateTicket.comp.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Form, Button} from 'react-bootstrap'
 
-export const UpdateTicket = () => {
+export const UpdateTicket = ({msg, handleOnChange, handleOnSubmit}) => {
     return(
-        <Form>
+        <Form onSubmit={handleOnSubmit}>
             <Form.Label>Reply</Form.Label>
             <br />
             <Form.Text>Reply or update your ticket here</Form.Text>
             <Form.Control 
-            as='textarea'
-            row='5'
-            name='detail'/>
+            value={msg}
+            onChange={handleOnChange}
+            as='textarea' row='5' name='detail'/>
             <div className='text-end mt-3 mb-3'>
                <Button variant='info' type='submit'>Reply</Button> 
             </div>
             
         </Form>
     )
+}
+
+UpdateTicket.propTypes = {
+    handleOnChange: PropTypes.func.isRequired,
+    handleOnSubmit: PropTypes.func.isRequired,
+    msg: PropTypes.string.isRequired,
 }
--- src/pages/ticket/Ticket.page.js
+++ src/pages/ticket/Ticket.page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Container, Row, Col, Button} from 'react-bootstrap'
 import { PageBreadcrumb } from '../../components/breadcrumb/Breadcrumb.comp'
 import tickets from '../../assets/data/dummy-tickets.json'
@@ -8,6 +8,18 @@ import { UpdateTicket } from '../../components/update-ticket/UpdateTicket.comp'
 const ticket = tickets[0]
 
 export const Ticket = () => {
+    const [message, setMessage] = useState('')
+    useEffect(() => {
+        
+    }, [message])
+
+    const handleOnChange = e => {
+        setMessage(e.target.value)
+    }
+
+    const handleOnSubmit = () => {
+        alert('Form Submitted')
+    }
     return(
         <Container>
             <Row>
@@ -18,7 +30,7 @@ export const Ticket = () => {
             <Row>
                 <Col className='text-weight-bolder text-secondary'>
                     <div className='subject'>Subject: {ticket.subject}</div>
-                    <div className='date'>Date: {ticket.addedAt}</div>
+                    <div className='date'>Ticket Opened On: {ticket.addedAt}</div>
                     <div className='status'>Status: {ticket.status}</div>
                 </Col>
                 <Col className='text-end'>
@@ -34,7 +46,10 @@ export const Ticket = () => {
 
             <Row className='mt-4'>
                 <Col>
-                    <UpdateTicket />
+                    <UpdateTicket msg={message} 
+                    handleOnChange={handleOnChange}
+                    handleOnSubmit={handleOnSubmit}
+                    />
                 </Col>
             </Row>
         </Container>
